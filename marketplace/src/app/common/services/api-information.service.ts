967e7d06dc4f121759c88650a847b3933b3ff379
jonas <jonas.knebel@gmx.de>
2023-12-16 21:53:14 +0100

    added alias paths and json file for matrix data

--- marketplace/src/app/common/services/api-information.service.ts
+++ marketplace/src/app/common/services/api-information.service.ts
@@ -1,8 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiInformationService {
-  private http = inject(HttpService);
+  private http = inject(HttpClient);
+
+  getApiMatrixData(): Observable<any> {
+    return this.http.get('assets/api-matrix');
+  }
 }
