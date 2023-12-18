7db16ae683c0bf66db86777ae39dd3491d4841d5
Laura Schmidt <lauschmidt.ls@gmail.com>
2023-12-18 17:21:04 +0100

    read api information from json

--- marketplace/src/app/common/services/api-data.service.ts
+++ marketplace/src/app/common/services/api-data.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import {ApiDetails} from "../../pages/home-page/home-page.component";
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,12 @@ export class ApiDataService {
 
   getApiMatrixData(): Observable<any> {
     return this.http.get(this.basePath + 'api-matrix.json', {
+      responseType: 'json',
+    });
+  }
+
+  getApiInformationData(): Observable<any> {
+    return this.http.get(this.basePath + 'api-information.json', {
       responseType: 'json',
     });
   }
--- marketplace/src/app/pages/home-page/home-page.component.ts
+++ marketplace/src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
 import {NgForOf, NgOptimizedImage} from "@angular/common";
 import {MatDividerModule} from '@angular/material/divider';
+import {ApiDataService} from "@common/services/api-data.service";
 
 @Component({
   selector: 'app-home-page',
@@ -16,31 +17,14 @@ import {MatDividerModule} from '@angular/material/divider';
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-  pensionAPI: ApiDetails = {
-    name: "Pension API",
-    shortDescription: "The FRIDA use case focuses on the provision of data for transparency in retirement provision, e.g. using a pension cockpit",
-    iconPath: "./assets/icons/pension-api.png"
-  }
-  healthCareAPI: ApiDetails = {
-    name: "Health-Care API",
-    shortDescription: "The FRIDA use case focuses on use cases in the healthcare sector, e.g. integrated contract and benefit management",
-    iconPath: "./assets/icons/health-care-api.png"
-  }
-  carClaimsAPI: ApiDetails = {
-    name: "Car-Claims API",
-    shortDescription: "The FRIDA use case focuses on the exchange of insurance data in the event of a claim, e.g. via wallet",
-    iconPath: "./assets/icons/car-claims-api.png"
-  }
-  cyberAPI: ApiDetails = {
-    name: "Cyber API",
-    shortDescription: "The FRIDA use case focuses on use cases in the context of cyber insurance, e.g. the standardization of key risk indicators and data exchange between third-party providers and insurers",
-    iconPath: "./assets/icons/cyber-api.png"
-  }
+  private apiService = inject(ApiDataService);
 
-  apiList: ApiDetails[] = [this.pensionAPI, this.healthCareAPI, this.carClaimsAPI, this.cyberAPI]
+  apiList: ApiDetails[] = [];
+
+  apiData = this.apiService.getApiInformationData().subscribe(apis => this.apiList = apis as ApiDetails[])
 }
 
-interface ApiDetails {
+export interface ApiDetails {
   name: string;
   shortDescription: string;
   iconPath: string;
