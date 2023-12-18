446bc5f5665234772c3dab5b40098a7165170369
Laura Schmidt <lauschmidt.ls@gmail.com>
2023-12-18 16:38:15 +0100

    implement first basic version of home screen

--- marketplace/src/app/pages/home-page/home-page.component.ts
+++ marketplace/src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+import {NgForOf, NgOptimizedImage} from "@angular/common";
+import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    NgForOf,
+    MatDividerModule,
+    NgOptimizedImage
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
+  pensionAPI: ApiDetails = {
+    name: "Pension API",
+    shortDescription: "The FRIDA use case focuses on the provision of data for transparency in retirement provision, e.g. using a pension cockpit"
+  }
+  healthCareAPI: ApiDetails = {
+    name: "Health-Care API",
+    shortDescription: "The FRIDA use case focuses on use cases in the healthcare sector, e.g. integrated contract and benefit management"
+  }
+  carClaimsAPI: ApiDetails = {
+    name: "Car-Claims API",
+    shortDescription: "The FRIDA use case focuses on the exchange of insurance data in the event of a claim, e.g. via wallet"
+  }
+  cyberAPI: ApiDetails = {
+    name: "Cyber API",
+    shortDescription: "The FRIDA use case focuses on use cases in the context of cyber insurance, e.g. the standardization of key risk indicators and data exchange between third-party providers and insurers"
+  }
 
+  apiList: ApiDetails[] = [this.pensionAPI, this.healthCareAPI, this.carClaimsAPI, this.cyberAPI]
+}
+
+interface ApiDetails {
+  name: string;
+  shortDescription: string;
 }
