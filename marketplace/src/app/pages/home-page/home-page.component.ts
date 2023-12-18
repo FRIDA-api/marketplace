e992b27a6f85da2f5396f81c71c4e64b0cc7ae35
Laura Schmidt <lauschmidt.ls@gmail.com>
2023-12-18 16:58:37 +0100

    add individual icons (change later)

--- marketplace/src/app/pages/home-page/home-page.component.ts
+++ marketplace/src/app/pages/home-page/home-page.component.ts
@@ -18,19 +18,23 @@ import {MatDividerModule} from '@angular/material/divider';
 export class HomePageComponent {
   pensionAPI: ApiDetails = {
     name: "Pension API",
-    shortDescription: "The FRIDA use case focuses on the provision of data for transparency in retirement provision, e.g. using a pension cockpit"
+    shortDescription: "The FRIDA use case focuses on the provision of data for transparency in retirement provision, e.g. using a pension cockpit",
+    iconPath: "./assets/icons/pension-api.png"
   }
   healthCareAPI: ApiDetails = {
     name: "Health-Care API",
-    shortDescription: "The FRIDA use case focuses on use cases in the healthcare sector, e.g. integrated contract and benefit management"
+    shortDescription: "The FRIDA use case focuses on use cases in the healthcare sector, e.g. integrated contract and benefit management",
+    iconPath: "./assets/icons/health-care-api.png"
   }
   carClaimsAPI: ApiDetails = {
     name: "Car-Claims API",
-    shortDescription: "The FRIDA use case focuses on the exchange of insurance data in the event of a claim, e.g. via wallet"
+    shortDescription: "The FRIDA use case focuses on the exchange of insurance data in the event of a claim, e.g. via wallet",
+    iconPath: "./assets/icons/car-claims-api.png"
   }
   cyberAPI: ApiDetails = {
     name: "Cyber API",
-    shortDescription: "The FRIDA use case focuses on use cases in the context of cyber insurance, e.g. the standardization of key risk indicators and data exchange between third-party providers and insurers"
+    shortDescription: "The FRIDA use case focuses on use cases in the context of cyber insurance, e.g. the standardization of key risk indicators and data exchange between third-party providers and insurers",
+    iconPath: "./assets/icons/cyber-api.png"
   }
 
   apiList: ApiDetails[] = [this.pensionAPI, this.healthCareAPI, this.carClaimsAPI, this.cyberAPI]
@@ -39,4 +43,5 @@ export class HomePageComponent {
 interface ApiDetails {
   name: string;
   shortDescription: string;
+  iconPath: string;
 }
