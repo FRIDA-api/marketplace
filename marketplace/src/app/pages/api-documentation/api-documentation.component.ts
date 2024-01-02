64090748291eca81cda02700f67674a537b3870a
jonas <jonas.knebel@gmx.de>
2024-01-02 20:33:18 +0100

    started with implementation of sidebar of the component

--- marketplace/src/app/pages/api-documentation/api-documentation.component.ts
+++ marketplace/src/app/pages/api-documentation/api-documentation.component.ts
@@ -1,27 +1,58 @@
 import { CommonModule, DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, Inject, Input, OnInit, ViewChild, inject,  } from '@angular/core';
-import { BehaviorSubject, distinctUntilChanged, filter, tap } from 'rxjs';
-import  SwaggerUI  from "swagger-ui"
-import SwaggerUIPlugin from "swagger-ui"
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+  inject,
+} from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import SwaggerUI from 'swagger-ui';
+
+type Api = {
+  url: string;
+};
+type Category = {
+  categoryName: string;
+  apis: Api[];
+};
+type CompanyInformation = {
+  companyName: string;
+  categories: Category[];
+};
 
 @Component({
   selector: 'app-api-documentation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatExpansionModule],
   templateUrl: './api-documentation.component.html',
   styleUrl: './api-documentation.component.scss',
 })
-export class ApiDocumentationComponent implements OnInit{
-
+export class ApiDocumentationComponent implements OnInit {
   //This parameter comes from the router path
   @Input() apiPathParameter!: string;
-  private document = inject(DOCUMENT)
-  
-  ngOnInit(): void {
-      SwaggerUI({
-        url: '/assets/api/FRIDA_PensionInformation_OA3_full_en.yaml',
-        domNode: this.document.getElementById('swagger-ui')
-      });
+  private document = inject(DOCUMENT);
 
+  public companies: CompanyInformation[] = [1, 2, 3, 4, 5].map((index) => {
+    let api = {
+      companyName: 'HDI' + index,
+      categories: [
+        { categoryName: 'PensionApi' + index, apis: [{ url: 'test' + index }] },
+        {
+          categoryName: 'PensionApi' + +index * 2,
+          apis: [{ url: 'test' + index }],
+        },
+      ],
+    };
+    return api;
+  });
+
+  public panelOpenState = false;
+  ngOnInit(): void {
+    console.log(this.apiPathParameter);
+    SwaggerUI({
+      url: '/assets/api/FRIDA_PensionInformation_OA3_full_en.yaml',
+      domNode: this.document.getElementById('swagger-ui'),
+    });
   }
 }
