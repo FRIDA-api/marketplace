a6a9328efc52c956ec4f4974bdb396acfe11993a
Tom Schmanke <tomschmanke@gmx.de>
2024-01-11 18:16:34 +0100

    Make header sticky with fading background for large and larger screens

--- marketplace/src/app/app.component.ts
+++ marketplace/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/header/header.component";
+import { fromEvent } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-root',
@@ -10,6 +12,20 @@ import { HeaderComponent } from "./core/header/header.component";
     styleUrl: './app.component.scss',
     imports: [CommonModule, RouterOutlet, HeaderComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    @ViewChild('main', {static: true}) el!: ElementRef;
+
+    private readonly destroyRef = inject(DestroyRef);
+
+    scrollPostion = 0;
+  
+    ngOnInit() {
+      fromEvent(this.el.nativeElement, 'scroll').pipe(
+        takeUntilDestroyed(this.destroyRef)
+      ).subscribe((_) => {
+        this.scrollPostion = this.el.nativeElement.scrollTop;
+      })
+    }
 
 }
