ca5ca0c7476a5726d3ebf8ef1766859679d43c30
Laura Schmidt <lauschmidt.ls@gmail.com>
2024-01-12 12:41:05 +0100

    change path to /api-explorer

--- marketplace/src/app/app.routes.ts
+++ marketplace/src/app/app.routes.ts
@@ -1,15 +1,14 @@
 import { Routes } from '@angular/router';
 
 import { ApiDocumentationComponent } from './pages/api-documentation/api-documentation.component';
-import { ApiMatrixComponent } from './pages/api-landscape/api-matrix.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { DataModelComponent } from './pages/data-model/data-model.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'api-catalogue', component: HomePageComponent },
-  { path: 'api-matrix', component: ApiMatrixComponent },
-  { path: 'apis/:apiPathParameter', component: ApiDocumentationComponent },
+  { path: 'api-explorer', component: ApiDocumentationComponent },
+  { path: 'api-explorer/:apiPathParameter', component: ApiDocumentationComponent },
   { path: 'data-model', component: DataModelComponent },
   { path: '**', redirectTo: '' },
 ];
