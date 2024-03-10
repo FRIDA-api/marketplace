98035f5fb1f182ece3d152b7e80f259d0a484fea
jonas <jonas.knebel@gmx.de>
2024-03-10 13:43:26 +0100

    base Implementation for translations

--- marketplace/src/app/app.config.ts
+++ marketplace/src/app/app.config.ts
@@ -1,18 +1,45 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
+import {
+  HttpBackend,
+  HttpClient,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
+import {
+  provideClientHydration,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { routes } from './app.routes';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
+export function createTranslateLoader(http: HttpBackend) {
+  return new TranslateHttpLoader(
+    new HttpClient(http),
+    './assets/i18n/',
+    '.json'
+  );
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withFetch()),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        defaultLanguage: 'de',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpBackend],
+        },
+      }),
+    ]),
     provideClientHydration(
       withHttpTransferCacheOptions({
-        includePostRequests: true
+        includePostRequests: true,
       })
     ),
     provideAnimations(),
--- marketplace/src/app/core/header/header.component.ts
+++ marketplace/src/app/core/header/header.component.ts
@@ -3,22 +3,31 @@ import {
   ElementRef,
   Input,
   OnChanges,
+  OnInit,
   ViewChild,
+  inject,
 } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent implements OnChanges {
+export class HeaderComponent implements OnInit, OnChanges {
   @Input() scrollPosition!: number;
-
   @ViewChild('header', { static: true }) headerRef!: ElementRef;
 
+  public translate = inject(TranslateService);
+
+  ngOnInit(): void {
+    this.translate.addLangs(['en', 'de']);
+    const browserLang = this.translate.getBrowserLang() ?? 'de';
+    this.translate.use(browserLang.match(/en|de/) ? browserLang : 'en');
+  }
   ngOnChanges() {
     if (this.scrollPosition === 0) {
       this.headerRef.nativeElement.setAttribute(
