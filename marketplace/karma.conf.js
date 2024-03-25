37f3f6fc4351d961afeffc7bae193e76de6d7376
Jonas Knebel <jonas.knebel@gmx.de>
2024-03-25 14:36:08 +0100

    fixed yml

--- marketplace/karma.conf.js
+++ marketplace/karma.conf.js
@@ -40,7 +40,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ["ChromiumHeadless"],
     singleRun: false,
     restartOnFileChange: true,
   });
