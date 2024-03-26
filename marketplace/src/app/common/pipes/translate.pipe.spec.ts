72201e30e86de9615dd09e021895c784e0011c21
Tom Schmanke <tomschmanke@gmx.de>
2024-03-26 21:59:47 +0100

    Add unit test for translate pipe

--- marketplace/src/app/common/pipes/translate.pipe.spec.ts
+++ marketplace/src/app/common/pipes/translate.pipe.spec.ts
@@ -1,8 +1,31 @@
 import { Translate } from './translate.pipe';
 
 describe('TranslatePipe', () => {
+
   it('create an instance', () => {
     const pipe = new Translate();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should choose correct translation', () => {
+    const pipe = new Translate();
+    expect(pipe.transform('de', 'german', 'english')).toEqual('german');
+    expect(pipe.transform('en', 'german', 'english')).toEqual('english');
+  });
+
+  it('should choose german as standard translation', () => {
+    const pipe = new Translate();
+    expect(pipe.transform('', 'german', 'english')).toEqual('german');
+    expect(pipe.transform('ff', 'german', 'english')).toEqual('german');
+  });
+
+  it('should handle empty strings', () => {
+    const pipe = new Translate();
+    expect(pipe.transform('', '', '')).toEqual('');
+    expect(pipe.transform('de', '', '')).toEqual('');
+    expect(pipe.transform('de', 'german', '')).toEqual('german');
+    expect(pipe.transform('', 'german', '')).toEqual('german');
+    expect(pipe.transform('', 'german', 'english')).toEqual('german');
+    expect(pipe.transform('de', '', 'english')).toEqual('');
   });
 });
