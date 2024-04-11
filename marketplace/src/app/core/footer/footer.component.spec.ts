b9b0331ff88389729c7c5dc37ce7e0fefd88d47a
Tom Schmanke <tomschmanke@gmx.de>
2024-04-11 23:00:20 +0200

    Broken test for pair fixing

--- marketplace/src/app/core/footer/footer.component.spec.ts
+++ marketplace/src/app/core/footer/footer.component.spec.ts
@@ -1,20 +1,27 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import {MockBuilder, MockedComponentFixture, MockRender, MockService} from "ng-mocks";
+import {TranslateModule} from "@ngx-translate/core";
+import {ActivatedRoute} from "@angular/router";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+  let fixture: MockedComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FooterComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    MockBuilder(FooterComponent)
+      .keep(TranslateModule.forRoot())
+      .keep(HttpClientTestingModule)
+      .provide({
+        provide: ActivatedRoute,
+        useValue: MockService(ActivatedRoute),
+      })
+      .then(() => {
+        fixture = MockRender(FooterComponent);
+        component = fixture.point.componentInstance;
+      })
   });
 
   it('should create', () => {
--- marketplace/src/app/pages/home-page/home-page.component.spec.ts
+++ marketplace/src/app/pages/home-page/home-page.component.spec.ts
@@ -1,21 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomePageComponent } from './home-page.component';
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MockBuilder, MockedComponentFixture, MockRender, MockService} from "ng-mocks";
+import {TranslateModule} from "@ngx-translate/core";
+import {ActivatedRoute} from "@angular/router";
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
-  let fixture: ComponentFixture<HomePageComponent>;
+  let fixture: MockedComponentFixture<HomePageComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HomePageComponent, HttpClientTestingModule]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(HomePageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    MockBuilder(HomePageComponent)
+      .keep(TranslateModule.forRoot())
+      .keep(HttpClientTestingModule)
+      .provide({
+        provide: ActivatedRoute,
+        useValue: MockService(ActivatedRoute),
+      })
+      .then(() => {
+        fixture = MockRender(HomePageComponent);
+        component = fixture.point.componentInstance;
+      })
   });
 
   it('should create', () => {
