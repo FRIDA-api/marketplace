ac2656182f7e5dde26258c923b6381db31b160e6
Laura Schmidt <lauschmidt.ls@gmail.com>
2024-04-12 11:16:10 +0200

    remove api-explorer from path

--- marketplace/src/app/app.routes.ts
+++ marketplace/src/app/app.routes.ts
@@ -5,9 +5,8 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'api-explorer', redirectTo: 'api-explorer/', pathMatch: 'full' },
   {
-    path: 'api-explorer/:apiPathParameter',
+    path: ':apiPathParameter',
     component: ApiDocumentationComponent,
   },
   { path: '**', redirectTo: '' },
