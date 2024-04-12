f8d4f2e530e4d98b3cddeb1efa843216299e5416
jonas <jonas.knebel@gmx.de>
2024-04-12 13:09:44 +0200

    started implementation of download page

--- marketplace/src/app/app.routes.ts
+++ marketplace/src/app/app.routes.ts
@@ -2,13 +2,24 @@ import { Routes } from '@angular/router';
 
 import { ApiDocumentationComponent } from './pages/api-documentation/api-documentation.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { DownloadComponent } from './pages/home-page/download/download.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'api-explorer', redirectTo: 'api-explorer/', pathMatch: 'full' },
   {
     path: 'api-explorer/:apiPathParameter',
-    component: ApiDocumentationComponent,
+    loadComponent: () =>
+      import('./pages/api-documentation/api-documentation.component').then(
+        () => ApiDocumentationComponent
+      ),
+  },
+  {
+    path: 'downloads',
+    loadComponent: () =>
+      import('./pages/home-page/download/download.component').then(
+        () => DownloadComponent
+      ),
   },
   { path: '**', redirectTo: '' },
 ];
--- marketplace/src/app/pages/home-page/download/download.component.ts
+++ marketplace/src/app/pages/home-page/download/download.component.ts
@@ -1,15 +1,15 @@
-import {Component, Input} from '@angular/core';
-import {ApiDownloadModel} from "@common/models/api-download.model";
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ApiDownloadModel } from '@common/models/api-download.model';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-download',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule, NgOptimizedImage],
   templateUrl: './download.component.html',
-  styleUrl: './download.component.scss'
+  styleUrl: './download.component.scss',
 })
 export class DownloadComponent {
-
   @Input() input?: ApiDownloadModel | null;
-
 }
