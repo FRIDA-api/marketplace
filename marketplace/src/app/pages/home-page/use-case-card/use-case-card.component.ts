3be13dd35295b558544eff5860d1d9b102a25a2f
jonas <jonas.knebel@gmx.de>
2024-04-12 13:21:15 +0200

    fixed another merge bug

--- marketplace/src/app/pages/home-page/use-case-card/use-case-card.component.ts
+++ marketplace/src/app/pages/home-page/use-case-card/use-case-card.component.ts
@@ -1,27 +1,24 @@
-import {Component, computed, inject, input, Signal} from '@angular/core';
-import { ApiInformationModel } from "@common/models/api-information.model";
-import { TagModel } from "@common/models/tag.model";
-import { NgOptimizedImage } from "@angular/common";
-import { RouterLink } from "@angular/router";
-import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import { Component, computed, inject, input, Signal } from '@angular/core';
+import { ApiInformationModel } from '@common/models/api-information.model';
+import { TagModel } from '@common/models/tag.model';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-use-case-card',
   standalone: true,
-  imports: [
-    NgOptimizedImage,
-    RouterLink,
-    TranslateModule
-  ],
+  imports: [NgOptimizedImage, RouterLink, TranslateModule],
   templateUrl: './use-case-card.component.html',
-  styleUrl: './use-case-card.component.scss'
+  styleUrl: './use-case-card.component.scss',
 })
 export class UseCaseCardComponent {
-
   protected readonly translate = inject(TranslateService);
 
   apiData = input.required<ApiInformationModel>();
   tagsData = input.required<TagModel[]>();
 
-  tagComputed: Signal<TagModel[]> = computed(() => this.tagsData().filter((tag) => this.apiData().tags.includes(tag.id)));
+  tagComputed: Signal<TagModel[]> = computed(() =>
+    this.tagsData().filter((tag) => this.apiData().tags.includes(tag.id))
+  );
 }
