5a5815d1e237454cc3a8bb1cdf1c2fab78c9a617
Laura Schmidt <lauschmidt.ls@gmail.com>
2024-04-12 14:52:46 +0200

    mock data

--- marketplace/src/app/pages/api-documentation/api-documentation.component.spec.ts
+++ marketplace/src/app/pages/api-documentation/api-documentation.component.spec.ts
@@ -1,20 +1,41 @@
 import { ComponentFixture } from '@angular/core/testing';
 
 import { ApiDocumentationComponent } from './api-documentation.component';
-import {MockBuilder, MockRender} from "ng-mocks";
+import {MockBuilder, MockedComponentFixture, MockRender, MockService} from "ng-mocks";
 import {TranslateModule} from "@ngx-translate/core";
+import {ActivatedRoute} from "@angular/router";
 
 describe('ApiDocumentationComponent', () => {
   let component: ApiDocumentationComponent;
-  let fixture: ComponentFixture<ApiDocumentationComponent>;
+  let fixture: MockedComponentFixture;
+  const tagDataMock = [{
+    "tagId": "pension",
+    "tagNameDE": "Pension",
+    "tagNameEN": "Pension",
+    "tagColor": "#6FBEAB"
+  }];
+  const apiDataMock = {
+    "id": "pension-api",
+    "nameDE": "PensionAPI",
+    "nameEN": "PensionAPI",
+    "descriptionDE": "Bereitstellung von Daten zur Transparenz in der Altersvorsorge z.B. mithilfe eines Rentencockpits.",
+    "descriptionEN": "Provision of data for transparency in pension provision, e.g. with the help of a pension cockpit.",
+    "iconPath": "./assets/icons/icon-pensionapi.svg",
+    "tags": [
+      "pension"
+    ]
+  };
 
   beforeEach(async () => {
     return MockBuilder(ApiDocumentationComponent)
       .keep(TranslateModule.forRoot())
+      .provide({
+        provide: ActivatedRoute,
+        useValue: MockService(ActivatedRoute),
+      })
       .then(() => {
-        fixture = MockRender(ApiDocumentationComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        fixture = MockRender(ApiDocumentationComponent, {apiInformation: apiDataMock, tagData: tagDataMock});
+        component = fixture.point.componentInstance;
       })
   });
 
