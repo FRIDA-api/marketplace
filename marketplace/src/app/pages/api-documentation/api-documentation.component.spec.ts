247bcc91e85e51e42343e7e48d2165e06ccaffd5
Laura Schmidt <lauschmidt.ls@gmail.com>
2024-04-12 15:15:58 +0200

    finally fix test

--- marketplace/src/app/pages/api-documentation/api-documentation.component.spec.ts
+++ marketplace/src/app/pages/api-documentation/api-documentation.component.spec.ts
@@ -2,38 +2,47 @@ import { ApiDocumentationComponent } from './api-documentation.component';
 import {MockBuilder, MockedComponentFixture, MockRender, MockService} from "ng-mocks";
 import {TranslateModule} from "@ngx-translate/core";
 import {ActivatedRoute} from "@angular/router";
+import {CommonModule} from "@angular/common";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 describe('ApiDocumentationComponent', () => {
   let component: ApiDocumentationComponent;
   let fixture: MockedComponentFixture;
-  const apiPathParameterMock = "pension-api"
-  const apiInformationMock = {
-    "id": "pension-api",
-    "nameDE": "PensionAPI",
-    "nameEN": "PensionAPI",
-    "descriptionDE": "Bereitstellung von Daten zur Transparenz in der Altersvorsorge z.B. mithilfe eines Rentencockpits.",
-    "descriptionEN": "Provision of data for transparency in pension provision, e.g. with the help of a pension cockpit.",
-    "iconPath": "./assets/icons/icon-pensionapi.svg",
-    "tags": [
-      "pension"
-    ]
-  };
-  const tagDataMock = [{
-    "tagId": "pension",
-    "tagNameDE": "Pension",
-    "tagNameEN": "Pension",
-    "tagColor": "#6FBEAB"
-  }];
+
+  const mockData = {
+    apiPathParameter: "pension-api",
+    apiInformation: {
+      "id": "pension-api",
+      "nameDE": "PensionAPI",
+      "nameEN": "PensionAPI",
+      "descriptionDE": "Bereitstellung von Daten zur Transparenz in der Altersvorsorge z.B. mithilfe eines Rentencockpits.",
+      "descriptionEN": "Provision of data for transparency in pension provision, e.g. with the help of a pension cockpit.",
+      "iconPath": "./assets/icons/icon-pensionapi.svg",
+      "tags": [
+        "pension"
+      ]
+    },
+    tagData: [{
+      "tagId": "pension",
+      "tagNameDE": "Pension",
+      "tagNameEN": "Pension",
+      "tagColor": "#6FBEAB"
+    }]
+  }
 
   beforeEach(async () => {
     return MockBuilder(ApiDocumentationComponent)
       .keep(TranslateModule.forRoot())
+      .keep(MatTabsModule)
+      .keep(BrowserAnimationsModule)
       .provide({
         provide: ActivatedRoute,
         useValue: MockService(ActivatedRoute),
       })
       .then(() => {
-        fixture = MockRender(ApiDocumentationComponent, {apiPathParameter: apiPathParameterMock, apiInformation: apiInformationMock, tagData: tagDataMock});
+        fixture = MockRender(ApiDocumentationComponent, mockData);
         component = fixture.point.componentInstance;
       })
   });
