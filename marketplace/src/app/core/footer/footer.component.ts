f67f253340f5de574b72f271fd5d81a1ebcca942
Tom Schmanke <tom.schmanke@rewe-group.com>
2024-06-16 22:32:07 +0200

    Fix all accessibility issues for wcag a and aa (without the bypass block and contrast requirements)

--- marketplace/src/app/core/footer/footer.component.ts
+++ marketplace/src/app/core/footer/footer.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {TranslateModule} from "@ngx-translate/core";
 import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [
     TranslateModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    RouterLink
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
