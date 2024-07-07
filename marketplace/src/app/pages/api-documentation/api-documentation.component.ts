c4794cf504800223cdf0cdc8a9c4604875dee2c5
jonas <jonas.knebel@gmx.de>
2024-07-07 15:55:48 +0200

    fixed possible memory leaks

--- marketplace/src/app/pages/api-documentation/api-documentation.component.ts
+++ marketplace/src/app/pages/api-documentation/api-documentation.component.ts
@@ -1,22 +1,19 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 
-import {
-  Component,
-  Input,
-  inject, OnInit
-} from '@angular/core';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {Router, RouterModule} from '@angular/router';
+import { Component, Input, inject, OnInit, OnDestroy } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Router, RouterModule } from '@angular/router';
 
-import {MatTabChangeEvent, MatTabsModule} from "@angular/material/tabs";
-import {OverviewTabComponent} from "./overview-tab/overview-tab.component";
-import {TranslateModule, TranslateService} from "@ngx-translate/core";
-import {DocumentationTabComponent} from "./documentation-tab/documentation-tab.component";
-import {TagsApiService} from "@common/services/tags-api.service";
-import {UseCaseApiService} from "@common/services/use-case-api.service";
-import {ApiInformationModel} from "@common/models/api-information.model";
-import {TagModel} from "@common/models/tag.model";
-import {Title} from "@angular/platform-browser";
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
+import { OverviewTabComponent } from './overview-tab/overview-tab.component';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { DocumentationTabComponent } from './documentation-tab/documentation-tab.component';
+import { TagsApiService } from '@common/services/tags-api.service';
+import { UseCaseApiService } from '@common/services/use-case-api.service';
+import { ApiInformationModel } from '@common/models/api-information.model';
+import { TagModel } from '@common/models/tag.model';
+import { Title } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-api-documentation',
@@ -34,7 +31,7 @@ import {Title} from "@angular/platform-browser";
   templateUrl: './api-documentation.component.html',
   styleUrl: './api-documentation.component.scss',
 })
-export class ApiDocumentationComponent implements OnInit {
+export class ApiDocumentationComponent implements OnInit, OnDestroy {
   @Input() apiPathParameter: string | undefined;
 
   private readonly useCasesApi = inject(UseCaseApiService);
@@ -46,21 +43,36 @@ export class ApiDocumentationComponent implements OnInit {
 
   apiInformation: ApiInformationModel | undefined;
   tags: TagModel[] | undefined;
+  private subs = new Subscription();
 
   isApiDocumentationTabActive = false;
 
   ngOnInit() {
-    this.apiInformation = this.useCasesApi.getUseCaseInformation().find(api => api.id === this.apiPathParameter);
+    this.apiInformation = this.useCasesApi
+      .getUseCaseInformation()
+      .find((api) => api.id === this.apiPathParameter);
     if (this.apiInformation === undefined) {
-      this.router.navigateByUrl("");
+      this.router.navigateByUrl('');
       return;
     }
 
-    this.tags = this.tagsApi.getTagInformation().filter(tag => this.apiInformation!.tags.includes(tag.id));
+    this.tags = this.tagsApi
+      .getTagInformation()
+      .filter((tag) => this.apiInformation!.tags.includes(tag.id));
 
-    this.translate.get("API_OVERVIEW." + this.apiInformation.languageKey + ".HEADLINE").subscribe((translation: string) => {
-      this.titleService.setTitle(`API Documentation ${translation} - Marketplace - FRIDA`);
-    });
+    this.subs.add(
+      this.translate
+        .get('API_OVERVIEW.' + this.apiInformation.languageKey + '.HEADLINE')
+        .subscribe((translation: string) => {
+          this.titleService.setTitle(
+            `API Documentation ${translation} - Marketplace - FRIDA`
+          );
+        })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 
   onTabChange(event: MatTabChangeEvent) {
