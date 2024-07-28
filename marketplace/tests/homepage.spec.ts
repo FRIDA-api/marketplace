0eded51d74bb40d50e4e8e48baec598c6b6227d2
jonas <jonas.knebel@gmx.de>
2024-07-28 15:26:34 +0200

    fixed e2e test

--- marketplace/tests/homepage.spec.ts
+++ marketplace/tests/homepage.spec.ts
@@ -1,20 +1,27 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('homepage', () => {
-
   test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/');
   });
 
-  test('show use-case cards', async ({ page}) => {
-    await expect(page.locator('.use-case-card-container').getByRole('heading', { level: 2 })).toHaveCount(7);
-    await expect(page.locator('.use-case-card-container').getByRole('paragraph')).toHaveCount(7);
-    await expect(page.locator('.use-case-card-container').getByRole('link')).toHaveCount(7);
+  test('show use-case cards', async ({ page }) => {
+    await expect(
+      page
+        .locator('.use-case-card-container')
+        .getByRole('heading', { level: 2 })
+    ).toHaveCount(7);
+    await expect(
+      page.locator('.use-case-card-container').getByRole('paragraph')
+    ).toHaveCount(7);
+    await expect(
+      page.locator('.use-case-card-container').getByRole('link')
+    ).toHaveCount(7);
   });
 
-  test('click use-case card', async ({ page}) => {
-    const contentCard = page.getByRole('link', {name: 'PensionAPI'});
+  test('click use-case card', async ({ page }) => {
+    const contentCard = page.getByRole('link', { name: 'PensionAPI' });
     await contentCard.click();
-    await expect(page).toHaveURL('http://localhost:4200/pension-api')
+    await expect(page).toHaveURL('http://localhost:4200/apis/pension-api');
   });
 });
