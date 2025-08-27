706be740c2df131f11cdf6c7e4a47119060cc676
Jonas Knebel <jonas.knebel@gmx.de>
2025-08-27 10:42:42 +0200

    Angular 20 update

--- marketplace/src/app/app.config.server.ts
+++ marketplace/src/app/app.config.server.ts
@@ -1,5 +1,5 @@
+import { provideServerRendering } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 
